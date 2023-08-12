45f34197542101531d7bc13e511de2c828d4a3a3
tamhofung <tamhofung@pc2.tamhofung.uk>
2023-08-12 10:29:24 +0800

    added year test cases

--- test_main.py
+++ test_main.py
@@ -5,13 +5,14 @@ import sys
 
 @pytest.mark.parametrize("arg", [2001, 2003, 1998])
 def test_noraml(arg):
-    assert CheckLeap(2001) == False
+    assert CheckLeap(arg) == False
 
-def test_noraml2():
-    assert CheckLeap(2000) == True
+@pytest.mark.parametrize("arg", [2000, 2020, 0])
+def test_leap(arg):
+    assert CheckLeap(arg) == True
 
 def test_import(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: "2002")
     result = getUserInput()
-    assert result == 2001
+    assert result == 2002
 
