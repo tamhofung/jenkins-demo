f76fde25d2ff29859adfd67f7dda71ea067df7b8
tamhofung <tamhofung@pc2.tamhofung.uk>
2023-08-12 10:01:05 +0800

    add test case

--- test_main.py
+++ test_main.py
@@ -10,17 +10,6 @@ def test_import(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: "2002")
     result = getUserInput()
     assert result == 2002
-'''
-def test_normal2(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: "2002")
-    result = CheckLeap()
-    assert result == False
-def test_leap(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: "2000")
-    result = CheckLeap()
-    assert result == True
-def test_leap2(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: "2020")
-    result = CheckLeap()
-    assert result == True
-'''
+
+def test_noraml2():
+    assert CheckLeap(2000) == True
